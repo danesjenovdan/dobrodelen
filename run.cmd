8db4b1552df7b2e239984896a518e658a66fb370
LepkoQQ <lepko.san@gmail.com>
2019-07-08 12:15:27 +0200

    add run cmd for dev

---
+++ run.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set CMDER_START=%~dp0
+echo %CMDER_START%
+
+cmd /k "%ConEmuDir%\..\init.bat & cd front & npm run dev" -new_console:n:t:"nuxt"
+
+cmd /k "%ConEmuDir%\..\init.bat & cd back & venv\Scripts\activate.bat & python manage.py runserver" -new_console:s50V:n:t:"wagtail"
