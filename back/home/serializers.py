35fa0e21845d9a91c24b1b96b223fa5285ff0d14
LepkoQQ <lepko.san@gmail.com>
2019-07-09 16:05:47 +0200

    fix org serializer to allow uploading wagtail images

--- back/home/serializers.py
+++ back/home/serializers.py
@@ -1,11 +1,22 @@
-from rest_framework import serializers
+from rest_framework import serializers, fields
 from wagtail.images.api.fields import ImageRenditionField
+from wagtail.images import get_image_model
 
 from .models import Organization
 
 
+class ImageRenditionAndUploadField(ImageRenditionField):
+    def to_internal_value(self, data):
+        if data:
+            # defer image validation to rest framework image field
+            clean_data = fields.ImageField().to_internal_value(data)
+            # get wagtail image model and create the image
+            Image = get_image_model()
+            return Image.objects.create(title=clean_data._name, file=clean_data)
+
+
 class OrganizationSerializer(serializers.ModelSerializer):
-    cover_photo = ImageRenditionField("original")
+    cover_photo = ImageRenditionAndUploadField("original")
 
     class Meta:
         model = Organization
