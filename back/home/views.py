ae6d2ab3b3c3162a72f6b6d76ab640ecedc934f8
LepkoQQ <lepko.san@gmail.com>
2019-07-09 16:04:32 +0200

    allow creating organizations via api when not logged in

--- back/home/views.py
+++ back/home/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
+
 
 from .models import Organization
 from .serializers import OrganizationSerializer
@@ -10,3 +11,10 @@ class OrganizationViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.OrderingFilter,)
     ordering_fields = ("criteria__stars",)
     ordering = ("-criteria__stars",)
+
+    def get_permissions(self):
+        if self.action in ["list", "create"]:
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+        return [permission() for permission in permission_classes]
