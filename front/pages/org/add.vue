68ca7561b5dcd1ce62f7a4976c14bda0939fd797
LepkoQQ <lepko.san@gmail.com>
2019-08-05 16:55:38 +0200

    custom radio styles

--- front/pages/org/add.vue
+++ front/pages/org/add.vue
@@ -53,6 +53,28 @@
               </textarea>
             </div>
           </fieldset>
+          <fieldset>
+            <legend>Področja delovanja</legend>
+            <small class="form-text">
+              <span>* lahko izberete več možnosti</span>
+            </small>
+
+            <div class="custom-control custom-radio">
+              <input id="cr1" type="radio" name="cr" class="custom-control-input" />
+              <label class="custom-control-label" for="cr1">this custom radio</label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input id="cr2" type="radio" name="cr" class="custom-control-input" />
+              <label class="custom-control-label" for="cr2">other custom radio</label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input id="cr3" type="radio" name="cr" class="custom-control-input" />
+              <label class="custom-control-label d-flex align-items-center" for="cr3">
+                Drugo:
+                <input class="form-control" />
+              </label>
+            </div>
+          </fieldset>
         </form>
       </div>
     </div>
@@ -210,6 +232,69 @@ export default {
         display: block;
         font-size: 0.9375rem;
         padding: 0 2rem;
+      }
+    }
+
+    .custom-radio,
+    .custom-checkbox {
+      padding-left: 2.5rem;
+
+      .custom-control-label {
+        font-size: 1.85rem;
+        font-weight: 300;
+        height: 4rem;
+        display: flex;
+        align-items: center;
+
+        &::before,
+        &::after {
+          width: 1.75rem;
+          height: 1.75rem;
+          top: 1.125rem;
+          left: -2.5rem;
+          cursor: pointer;
+        }
+
+        &::before {
+          border-width: 2px;
+        }
+
+        &.d-flex {
+          .form-control {
+            margin-left: 1.5rem;
+            border: 0;
+            background: rgba(#f6f2f0, 0.4);
+            font-size: 1.5rem;
+            font-weight: 300;
+            padding: 0.5rem 1.75rem;
+            border-bottom: 2px solid rgba($blue, 0.3);
+            height: 4rem;
+
+            &,
+            &:focus {
+              outline: 0;
+              box-shadow: none;
+            }
+
+            &:focus {
+              border-bottom-color: $blue;
+            }
+          }
+        }
+      }
+
+      .custom-control-input ~ .custom-control-label::before {
+        border-color: $blue;
+      }
+
+      .custom-control-input:checked ~ .custom-control-label::before {
+        background-color: #fff;
+      }
+
+      .custom-control-input:checked ~ .custom-control-label::after {
+        background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-10 -10 20 20'%3e%3ccircle r='5.5' fill='%23#{str-slice(#{$blue}, 2)}'/%3e%3c/svg%3e");
+        background-size: contain;
+        background-position: center;
       }
     }
   }
