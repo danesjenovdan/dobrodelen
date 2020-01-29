36667f5feb5f7411659ec97f13341084b33e50f6
LepkoQQ <lepko.san@gmail.com>
2020-01-29 14:34:10 +0100

    move vscode config to workspace file, open workspace in run.cmd

--- run.cmd
+++ run.cmd
@@ -6,3 +6,5 @@ echo %CMDER_START%
 cmd /k "%ConEmuDir%\..\init.bat & cd front & npm run dev" -new_console:n:t:"nuxt"
 
 cmd /k "%ConEmuDir%\..\init.bat & cd back & venv\Scripts\activate.bat & python manage.py runserver" -new_console:s50V:n:t:"wagtail"
+
+start /b code dobrodelen.code-workspace -new_console:bn
