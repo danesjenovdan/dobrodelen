830051458f1d150d8961affd18da56fa28a9779f
LepkoQQ <lepko.san@gmail.com>
2023-10-18 14:47:12 +0200

    Revert "disable page locking"
    This reverts commit da3e11ebb83fb729cbabf9962966792feb7d574f.

--- front/server/middleware/unlock.js
+++ front/server/middleware/unlock.js
@@ -1,40 +1,36 @@
 export default defineEventHandler(async (event) => {
   const path = getRequestPath(event);
-  // const config = useRuntimeConfig();
-  // const password = config.unlockPassword;
+  const config = useRuntimeConfig();
+  const password = config.unlockPassword;
 
+  // if we are on the locked page and the query password is correct, redirect to the home page
   if (path.startsWith('/locked')) {
-    return sendRedirect(event, '/', 302);
+    const query = getQuery(event);
+    if (query.password === password) {
+      setCookie(event, 'dobrodelen_password', password);
+      return sendRedirect(event, '/', 302);
+    }
   }
 
-  // // if we are on the locked page and the query password is correct, redirect to the home page
-  // if (path.startsWith('/locked')) {
-  //   const query = getQuery(event);
-  //   if (query.password === password) {
-  //     setCookie(event, 'dobrodelen_password', password);
-  //     return sendRedirect(event, '/', 302);
-  //   }
-  // }
+  // if we are on the signup page with an edit key, dont do anything
+  if (path.startsWith('/organizacija/prijava')) {
+    const query = getQuery(event);
+    if (query.edit_id && query.edit_key) {
+      return;
+    }
+  }
 
-  // // if we are on the signup page with an edit key, dont do anything
-  // if (path.startsWith('/organizacija/prijava')) {
-  //   const query = getQuery(event);
-  //   if (query.edit_id && query.edit_key) {
-  //     return;
-  //   }
-  // }
-
-  // // we are either on the locked page with wrong password or any other page
-  // const cookies = parseCookies(event);
-  // if (cookies.dobrodelen_password === password) {
-  //   // if we are on the locked page and the cookie password is correct, redirect to the home page
-  //   if (path.startsWith('/locked')) {
-  //     return sendRedirect(event, '/', 302);
-  //   }
-  // } else {
-  //   // if the cookie password is incorrect, redirect to the locked page
-  //   if (!path.startsWith('/locked')) {
-  //     return sendRedirect(event, '/locked', 302);
-  //   }
-  // }
+  // we are either on the locked page with wrong password or any other page
+  const cookies = parseCookies(event);
+  if (cookies.dobrodelen_password === password) {
+    // if we are on the locked page and the cookie password is correct, redirect to the home page
+    if (path.startsWith('/locked')) {
+      return sendRedirect(event, '/', 302);
+    }
+  } else {
+    // if the cookie password is incorrect, redirect to the locked page
+    if (!path.startsWith('/locked')) {
+      return sendRedirect(event, '/locked', 302);
+    }
+  }
 });
